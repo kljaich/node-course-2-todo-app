8a97a6e23b29143837bf56780306451587f5fe79
Joseph Kljaich <kljaich@gmail.com>
2018-06-27 13:10:03 -0500

    Fix bugs associated with heroku deployment

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,9 @@
 var mongoose = require('mongoose');
 
-// Configure moongoose to use promises instead of callbacks
+// Configure mongoose to use promises instead of callbacks
 mongoose.Promise = global.Promise;
 
 // Open and manage database connection
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Todo');
 
-module.exports = { mongoose};
+module.exports = { mongoose };
